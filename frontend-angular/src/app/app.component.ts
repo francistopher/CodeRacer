20444f9b4d0937569be3ff93054e87ae380d4002
francistopher <francistopherrr@outlook.com>
2023-12-19 00:08:16 -0500

    UPDATE app.component .html and .ts for angular to display message

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'qa-angular';
+  title = 'QA';
+  greeting = {}
+  // constructor(private http: HttpClient) {
+  //   http.get('resource').subscribe(data => this.greeting = data)
+  // }
+  
 }
