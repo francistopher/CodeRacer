9d4ba922db7a893af8bb2c906e5e5da0ed2ec80a
francistopher <francistopherrr@outlook.com>
2023-12-20 22:05:15 -0500

    CREATE home and resource path

--- backend/src/main/java/com/example/QA.java
+++ backend/src/main/java/com/example/QA.java
@@ -5,13 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @RestController
 @SpringBootApplication
 public class QA {
+
     @RequestMapping("/")
-    String home() {
-        return "<h1>QA</h1><h2>Code Questions & Answers</h2>";
+    public String home() {
+        return "<h1>QA</h1>";
     }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> newHomePage() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "QA Backend");
+        return model;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(QA.class, args);
