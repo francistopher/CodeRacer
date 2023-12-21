8b48ffa154ed7b8cbd2defbfe26a8381c04469bd
francistopher <francistopherrr@outlook.com>
2023-12-20 22:21:28 -0500

    UPDATE app.component.ts to make an http request to spring boot backend

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
-  title = 'QA';
-  greeting = {}
-  // constructor(private http: HttpClient) {
-  //   http.get('resource').subscribe(data => this.greeting = data)
-  // }
+
+
+  export class AppComponent implements OnInit {
+    title: string = "Angular";
+    responseData: any = {};
   
-}
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      this.http.get('http://localhost:8080/resource').subscribe(data => {
+        console.log(data)
+        this.responseData = data;
+      })  
+    }
+
+
+    
+  
+  }
